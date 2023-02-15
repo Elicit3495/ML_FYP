decca9238c76f9f9bc055c0575b99edfc643996d
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2023-02-15 11:47:28 +0000

    a

--- conditional.py
+++ conditional.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[24]:
 
 
 import pandas as pd
@@ -11,7 +11,11 @@ import numpy as np
 import pgmpy
 import itertools
 from scipy.stats import chisquare
-from scipy.stats import power_divergence
+
+
+# In[2]:
+
+
 from pgmpy.estimators.CITests import chi_square
 # from pgmpy.factors.continuous.discretize import BaseDiscretizer
 
@@ -26,14 +30,6 @@ def csv_format_discrete(csv_file):
 #returns the csv_file in a pandas dataframe, formatted properly, discrete dataset only
 
 
-# In[ ]:
-
-
-def csv_format_discrete(csv_file):
-    df = pd.read_csv(csv_file, sep="\s+")
-    return df.drop([0])
-
-
 # In[4]:
 
 
@@ -42,13 +38,21 @@ def gaussian_reader(csv_file):
     return df
 
 
-# In[1]:
+# In[5]:
+
+
+df = csv_format_discrete(r'C:\Users\User\Documents\GitHub\ML_FYP\dataset\asia_10000.dat')
+dataframe = df
+df
+
+
+# In[37]:
 
 
 def chi_square_CI(df):
 #the null hypothesis is that they are independent of each other
-#if true, the p_value is higher than the significance test, we do not reject the null hypothesis
-#if false, the p_value is lower than the significance test, we reject the null hypothesis
+#if true, accept null hypothesis
+#if false, reject null hypothesis
     v = list(df)
     empty = []
     empty_1 = []
@@ -63,15 +67,17 @@ def chi_square_CI(df):
     for t in range(len(itertools_combinations)):
         empty.append(itertools_combinations[t])
     for x,y,i in zip(empty_1, empty_2, empty):
-        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.95, boolean=True) #returns chi, p_value, dof
+        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.95) #returns chi, p_value, dof
         empty_3.append([i , chisquare])
     for value in range(len(empty_3)):
         empty_4.append(value)
     for x,y in zip(empty_4, empty_3):
         empty_5.append([x,y])
     return empty_5
+#if the chi_squared test gives true, then p_value is > significance level, hence we accept the null hypothesis, false otherwise
 
 
+<<<<<<< HEAD
 # In[ ]:
 
 
@@ -141,6 +147,10 @@ def chi_square_CI_test(df):
 
 # In[ ]:
 
+=======
+# In[38]:
+>>>>>>> parent of 9e59f29 (a)
 
 
+chi_square_CI(df)
 
