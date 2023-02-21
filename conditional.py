dee078e72b3e591723ae4e0e0123a4576c0b830b
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2023-02-21 15:27:15 +0000

    0.05 sf

--- conditional.py
+++ conditional.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+    #!/usr/bin/env python
 # coding: utf-8
 
-# In[24]:
+# In[28]:
 
 
 import pandas as pd
@@ -10,11 +10,8 @@ import numpy as np
 # import bnlearn as bn
 import pgmpy
 import itertools
-from scipy.stats import chisquare
-
-
-# In[2]:
-
+from scipy.stats import chi2_contingency #check for independence between 2 variables
+from scipy.stats import power_divergence
 
 from pgmpy.estimators.CITests import chi_square
 # from pgmpy.factors.continuous.discretize import BaseDiscretizer
@@ -30,7 +27,15 @@ def csv_format_discrete(csv_file):
 #returns the csv_file in a pandas dataframe, formatted properly, discrete dataset only
 
 
-# In[4]:
+# In[5]:
+
+
+def csv_format_discrete(csv_file):
+    df = pd.read_csv(csv_file, sep="\s+")
+    return df.drop([0])
+
+
+# In[6]:
 
 
 def gaussian_reader(csv_file):
@@ -38,48 +43,11 @@ def gaussian_reader(csv_file):
     return df
 
 
-# In[5]:
+# In[118]:
 
 
-df = csv_format_discrete(r'C:\Users\User\Documents\GitHub\ML_FYP\dataset\asia_10000.dat')
-dataframe = df
-df
-
-
-# In[37]:
-
-
-def chi_square_CI(df):
-#the null hypothesis is that they are independent of each other
-#if true, accept null hypothesis
-#if false, reject null hypothesis
-    v = list(df)
-    empty = []
-    empty_1 = []
-    empty_2 = []
-    empty_3 = []
-    empty_4 = []
-    empty_5 = []
-    itertools_combinations = list(itertools.combinations(v, 2)) #finds every possible combination of list(df)
-    for i in itertools_combinations:
-        empty_1.append(i[0])
-        empty_2.append(i[1])
-    for t in range(len(itertools_combinations)):
-        empty.append(itertools_combinations[t])
-    for x,y,i in zip(empty_1, empty_2, empty):
-        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.95) #returns chi, p_value, dof
-        empty_3.append([i , chisquare])
-    for value in range(len(empty_3)):
-        empty_4.append(value)
-    for x,y in zip(empty_4, empty_3):
-        empty_5.append([x,y])
-    return empty_5
-#if the chi_squared test gives true, then p_value is > significance level, hence we accept the null hypothesis, false otherwise
-
-# In[ ]:
-
-
-def chi_square_CI_test(df):
+def chi2(df):
+#returns a tuple(chi, p_value, dof) if boolean = false
 #the null hypothesis is that they are independent of each other
 #if true, the p_value is higher than the significance test, we do not reject the null hypothesis
 #if false, the p_value is lower than the significance test, we reject the null hypothesis
@@ -97,58 +65,11 @@ def chi_square_CI_test(df):
     for t in range(len(itertools_combinations)):
         empty.append(itertools_combinations[t])
     for x,y,i in zip(empty_1, empty_2, empty):
-        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.95, boolean=False) #returns chi, p_value, dof
+        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.95, boolean=True) #returns chi, p_value, dof
         empty_3.append([i , chisquare])
     for value in range(len(empty_3)):
         empty_4.append(value)
     for x,y in zip(empty_4, empty_3):
         empty_5.append([x,y])
     return empty_5
-
-
-# In[22]:
-
-
-# df = csv_format_discrete(r'C:\Users\User\Documents\GitHub\ML_FYP\dataset\asia_10000.dat')
-
-
-# In[ ]:
-
-
-
-
-
-# In[24]:
-
-
-
-
-# In[27]:
-
-
-# k = power_divergence(v, lambda_="log-likelihood")
-# #returns statistics, p-value
-
-
-# # In[30]:
-
-
-# k_ = list(k)
-# k_
-
-
-# In[31]:
-
-
-30/66
-
-
-# In[ ]:
-
-
-
-# In[38]:
-
-
-chi_square_CI(df)
 
