bd7005659f7e06034f8937992cc0ec025aeb6b18
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2023-02-22 12:32:36 +0000

    working on word finder

--- conditional.py
+++ conditional.py
@@ -46,7 +46,7 @@ def gaussian_reader(csv_file):
 # In[118]:
 
 
-def chi2(df):
+def chi2bool(df):
 #returns a tuple(chi, p_value, dof) if boolean = false
 #the null hypothesis is that they are independent of each other
 #if true, the p_value is higher than the significance test, we do not reject the null hypothesis
@@ -65,11 +65,39 @@ def chi2(df):
     for t in range(len(itertools_combinations)):
         empty.append(itertools_combinations[t])
     for x,y,i in zip(empty_1, empty_2, empty):
-        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.95, boolean=True) #returns chi, p_value, dof
+        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.05, boolean=True) #returns chi, p_value, dof
         empty_3.append([i , chisquare])
     for value in range(len(empty_3)):
         empty_4.append(value)
     for x,y in zip(empty_4, empty_3):
         empty_5.append([x,y])
     return empty_5
+
+def chi2val(df):
+    #returns a tuple(chi, p_value, dof) if boolean = false
+#the null hypothesis is that they are independent of each other
+#if true, the p_value is higher than the significance test, we do not reject the null hypothesis
+#if false, the p_value is lower than the significance test, we reject the null hypothesis
+    v = list(df)
+    empty = []
+    empty_1 = []
+    empty_2 = []
+    empty_3 = []
+    empty_4 = []
+    empty_5 = []
+    itertools_combinations = list(itertools.combinations(v, 2)) #finds every possible combination of list(df)
+    for i in itertools_combinations:
+        empty_1.append(i[0])
+        empty_2.append(i[1])
+    for t in range(len(itertools_combinations)):
+        empty.append(itertools_combinations[t])
+    for x,y,i in zip(empty_1, empty_2, empty):
+        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.05, boolean=False) #returns chi, p_value, dof
+        empty_3.append([i , chisquare])
+    for value in range(len(empty_3)):
+        empty_4.append(value)
+    for x,y in zip(empty_4, empty_3):
+        empty_5.append([x,y])
+    return empty_5
+
 
