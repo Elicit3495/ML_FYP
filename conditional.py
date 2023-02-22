f6db58947754d990da3f92a651f581684da139a5
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2023-02-22 21:34:22 +0000

    new function to conditional

--- conditional.py
+++ conditional.py
@@ -1,7 +1,7 @@
-    #!/usr/bin/env python
+#!/usr/bin/env python
 # coding: utf-8
 
-# In[28]:
+# In[1]:
 
 
 import pandas as pd
@@ -17,7 +17,7 @@ from pgmpy.estimators.CITests import chi_square
 # from pgmpy.factors.continuous.discretize import BaseDiscretizer
 
 
-# In[3]:
+# In[2]:
 
 
 def csv_format_discrete(csv_file):
@@ -27,7 +27,7 @@ def csv_format_discrete(csv_file):
 #returns the csv_file in a pandas dataframe, formatted properly, discrete dataset only
 
 
-# In[5]:
+# In[3]:
 
 
 def csv_format_discrete(csv_file):
@@ -35,7 +35,7 @@ def csv_format_discrete(csv_file):
     return df.drop([0])
 
 
-# In[6]:
+# In[4]:
 
 
 def gaussian_reader(csv_file):
@@ -43,11 +43,11 @@ def gaussian_reader(csv_file):
     return df
 
 
-# In[118]:
+# In[88]:
 
 
 def chi2bool(df):
-#returns a tuple(chi, p_value, dof) if boolean = false
+#returns a tuple(chi2, p_value, dof) if boolean = false
 #the null hypothesis is that they are independent of each other
 #if true, the p_value is higher than the significance test, we do not reject the null hypothesis
 #if false, the p_value is lower than the significance test, we reject the null hypothesis
@@ -57,24 +57,31 @@ def chi2bool(df):
     empty_2 = []
     empty_3 = []
     empty_4 = []
-    empty_5 = []
     itertools_combinations = list(itertools.combinations(v, 2)) #finds every possible combination of list(df)
     for i in itertools_combinations:
         empty_1.append(i[0])
         empty_2.append(i[1])
+        
+    total_number_of_combinations = len(empty_1)
+    
     for t in range(len(itertools_combinations)):
         empty.append(itertools_combinations[t])
-    for x,y,i in zip(empty_1, empty_2, empty):
+        
+    for value in range(len(empty_1)):
+        empty_3.append(value)
+        
+    for x,y,i,j in zip(empty_1, empty_2, empty, empty_3):
         chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.05, boolean=True) #returns chi, p_value, dof
-        empty_3.append([i , chisquare])
-    for value in range(len(empty_3)):
-        empty_4.append(value)
-    for x,y in zip(empty_4, empty_3):
-        empty_5.append([x,y])
-    return empty_5
+        empty_4.append([j, i , chisquare])
+        
+    return empty_4
+
+
+# In[6]:
+
 
 def chi2val(df):
-    #returns a tuple(chi, p_value, dof) if boolean = false
+#returns a tuple(chi2, p_value, dof) if boolean = false
 #the null hypothesis is that they are independent of each other
 #if true, the p_value is higher than the significance test, we do not reject the null hypothesis
 #if false, the p_value is lower than the significance test, we reject the null hypothesis
@@ -84,20 +91,37 @@ def chi2val(df):
     empty_2 = []
     empty_3 = []
     empty_4 = []
-    empty_5 = []
     itertools_combinations = list(itertools.combinations(v, 2)) #finds every possible combination of list(df)
     for i in itertools_combinations:
         empty_1.append(i[0])
         empty_2.append(i[1])
+        
+    total_number_of_combinations = len(empty_1)
+    
     for t in range(len(itertools_combinations)):
         empty.append(itertools_combinations[t])
-    for x,y,i in zip(empty_1, empty_2, empty):
+        
+    for value in range(len(empty_1)):
+        empty_3.append(value)
+        
+    for x,y,i,j in zip(empty_1, empty_2, empty, empty_3):
         chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.05, boolean=False) #returns chi, p_value, dof
-        empty_3.append([i , chisquare])
-    for value in range(len(empty_3)):
-        empty_4.append(value)
-    for x,y in zip(empty_4, empty_3):
-        empty_5.append([x,y])
-    return empty_5
+        empty_4.append([j, i , chisquare])
+        
+    return empty_4
 
+
+# In[100]:
+
+
+def chi2return(df):
+    true_list = []
+    false_list = []
+    chi2 = chi2bool(df)
+    for i in chi2:
+        if True in i:
+            true_list.append(i)
+        if False in i:
+            false_list.append(i)
+    return true_list , false_list
 
