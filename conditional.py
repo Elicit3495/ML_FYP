9a38800c0defb8d11d9cc4d8328ce3d52be9f24c
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2023-02-25 16:20:17 +0000

    g2 test implemented

--- conditional.py
+++ conditional.py
@@ -12,7 +12,7 @@ import pgmpy
 import itertools
 from scipy.stats import chi2_contingency #check for independence between 2 variables
 from scipy.stats import power_divergence
-from pgmpy.estimators.CITests import independence_match
+from pgmpy.estimators.CITests import log_likelihood
 from pgmpy.estimators.CITests import chi_square
 # from pgmpy.factors.continuous.discretize import BaseDiscretizer
 
@@ -43,9 +43,10 @@ def gaussian_reader(csv_file):
     return df
 
 
-# In[88]:
+# In[5]:
 
 
+#how do we measure the consistensy
 def chi2bool(df):
 #returns a tuple(chi2, p_value, dof) if boolean = false
 #the null hypothesis is that they are independent of each other
@@ -114,7 +115,20 @@ def chi2val(df):
 # In[ ]:
 
 
-def chi2val(df):
+
+
+
+# In[7]:
+
+
+# def g2bool(df):
+    
+
+
+# In[10]:
+
+
+def g2(df):
 #returns a tuple(chi2, p_value, dof) if boolean = false
 #the null hypothesis is that they are independent of each other
 #if true, the p_value is higher than the significance test, we do not reject the null hypothesis
@@ -139,47 +153,13 @@ def chi2val(df):
         empty_3.append(value)
         
     for x,y,i,j in zip(empty_1, empty_2, empty, empty_3):
-        chisquare = chi_square(X=x, Y=y, Z=[], data=df, significance_level=0.05, boolean=False) #returns chi, p_value, dof
-        empty_4.append([j, i , chisquare])
+        g2 = log_likelihood(X=x, Y=y, Z=[], significance_level=0.05, boolean=True, data=df) 
+        empty_4.append([j, i , g2])
         
     return empty_4
 
 
-# In[2]:
-
-
-def ind2bool(df):
-#returns a tuple(chi2, p_value, dof) if boolean = false
-#the null hypothesis is that they are independent of each other
-#if true, the p_value is higher than the significance test, we do not reject the null hypothesis
-#if false, the p_value is lower than the significance test, we reject the null hypothesis
-    v = list(df)
-    empty = []
-    empty_1 = []
-    empty_2 = []
-    empty_3 = []
-    empty_4 = []
-    itertools_combinations = list(itertools.combinations(v, 2)) #finds every possible combination of list(df)
-    for i in itertools_combinations:
-        empty_1.append(i[0])
-        empty_2.append(i[1])
-        
-    total_number_of_combinations = len(empty_1)
-    
-    for t in range(len(itertools_combinations)):
-        empty.append(itertools_combinations[t])
-        
-    for value in range(len(empty_1)):
-        empty_3.append(value)
-        
-    for x,y,i,j in zip(empty_1, empty_2, empty, empty_3):
-        ind_match = independence_match(X=x, Y=y, Z=[], independencies = xy , data=df) #returns chi, p_value, dof
-        empty_4.append([j, i , ind_match])
-        
-    return empty_4
-
-
-# In[100]:
+# In[9]:
 
 
 def chi2return(df):
@@ -193,4 +173,31 @@ def chi2return(df):
         if False in i:
             false_list.append(i)
     return false_list
+
+
+# In[12]:
+
+
+def g2return(df):
+    true_list = []
+    false_list = []
+    g2_return = g2(df)
+    for i in g2_return:
+        if True in i:
+            true_list.append(i)
+        if False in i:
+            false_list.append(i)
+    return false_list
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
