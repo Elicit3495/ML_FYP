1e2375da19c37174edb75b8bb0479ea30b19914e
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2023-04-02 00:53:14 +0100

    a

---
+++ chi2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import pandas as pd
+import numpy as np
+import itertools
+
+
+# In[1]:
+
+
+print(1+1+2)
+
+
+# In[3]:
+
+
+print("hello world")
+
+
+# In[4]:
+
+
+v = [123,32,424,324,312,341234123,412]
+
+
+# In[9]:
+
+
+k = list(itertools.combinations(v, 2))
+
+
+# In[13]:
+
+
+def helloworld():
+    return('hello world!!!!!')
+
+
+# In[14]:
+
+
+helloworld()
+
+
+# In[15]:
+
+
+def noworld():
+    print('no world')
+
+
+# In[17]:
+
+
+noworld()
+
+
+# In[ ]:
+
+
+
+
